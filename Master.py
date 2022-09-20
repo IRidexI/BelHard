d076e27c79fbad5726f63c58b39eeaf89794c3df
Ridex <ridexGumesPresent@gmail.com>
2022-09-20 13:52:39 +0300

    add simple calculator

--- Master.py
+++ Master.py
@@ -2,3 +2,9 @@ print("Life is a quality that distinguishes matter that has biological processes
       "self-sustaining processes,\n from that which does not, and is defined by the capacity for growth, reaction to "
       "stimuli, metabolism, energy transformation,\n and reproduction. Various forms of life exist, such as plants, "
       "animals, fungi, protists, archaea, and bacteria.\n Biology is the science that studies life.")
+
+num_1 = input("Enter first number: ")
+num_2 = input("Enter second number: ")
+result = int(num_1) + int(num_2)
+
+print(result)
