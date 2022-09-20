13b8c863a277fd37d751bd33d8c43971cdbab033
Ridex <ridexGumesPresent@gmail.com>
2022-09-20 14:10:19 +0300

    added a little description

--- Main.py
+++ Main.py
@@ -1,10 +1,16 @@
+# Just Text
+
 print("Life is a quality that distinguishes matter that has biological processes, such as signaling and "
       "self-sustaining processes,\n from that which does not, and is defined by the capacity for growth, reaction to "
       "stimuli, metabolism, energy transformation,\n and reproduction. Various forms of life exist, such as plants, "
       "animals, fungi, protists, archaea, and bacteria.\n Biology is the science that studies life.")
+
+# Simple calculator
 
 num_1 = input("Enter first number: ")
 num_2 = input("Enter second number: ")
 result = float(num_1) + float(num_2)
 
 print(result)
+
+# Something
