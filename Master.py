6b650b876294802a95727f1bc1c2d30a6e2cde6d
Ridex <ridexGumesPresent@gmail.com>
2022-09-20 10:24:40 +0300

    add Text to Master.py

---
+++ Master.py
@@ -0,0 +1,4 @@
+print("Life is a quality that distinguishes matter that has biological processes, such as signaling and "
+      "self-sustaining processes,\n from that which does not, and is defined by the capacity for growth, reaction to "
+      "stimuli, metabolism, energy transformation,\n and reproduction. Various forms of life exist, such as plants, "
+      "animals, fungi, protists, archaea, and bacteria.\n Biology is the science that studies life.")
