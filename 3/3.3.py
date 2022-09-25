f7beb1dace73763580d79693fdcf4583cfc2954a
Ridex <ridexGumesPresent@gmail.com>
2022-09-25 18:32:56 +0300

    add 3.3

--- 3/3.3.py
+++ 3/3.3.py
@@ -1,10 +1,12 @@
-users = {
-    1: {
-        'name': 'name1',
-        'email': 'email1'
-    },
-    2: {
-        'name': 'name2',
-        'email': 'email2'
-    }
-}
+
+# expected result:
+# users = {
+#     1: {
+#         'name': 'name1',
+#         'email': 'email1'
+#     },
+#     2: {
+#         'name': 'name2',
+#         'email': 'email2'
+#     }
+# }
