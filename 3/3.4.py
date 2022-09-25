9986d28768183c1f20aeca7d094b38f4146c29ba
Ridex <ridexGumesPresent@gmail.com>
2022-09-25 19:09:53 +0300

    add 3.4

--- 3/3.4.py
+++ 3/3.4.py
@@ -2,6 +2,6 @@ n = int(input("Enter n: "))
 numbers = {}
 for i in range(n):
     numb = input("Enter numbers: ")
-    numbers[i] = {"Numbers": numbers}
+    numbers[i] = {"Numbers": numb}
 
 print(max(numbers))
