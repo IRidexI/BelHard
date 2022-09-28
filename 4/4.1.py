b8f5c371d925601e0a84545204111e3d6735f223
Ridex <ridexGumesPresent@gmail.com>
2022-09-28 15:56:34 +0300

    A piece of 4.1

--- 4/4.1.py
+++ 4/4.1.py
@@ -1,6 +1,8 @@
-Number1 = int(input("Enter the range number: "))
+Number1 = int(input("Enter the first number: "))
 Number2 = int(input("Enter second number: "))
 Number3 = int(input("Enter last number: "))
 
 if Number1 % Number2 == 0 and Number1 > Number3:
-
+    print(Number1)
+else:
+    print("error")
