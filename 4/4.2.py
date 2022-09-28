a4da47fbc3b120cea3b960ac9947a42a7b4de832
Ridex <ridexGumesPresent@gmail.com>
2022-09-28 14:31:33 +0300

    4.2

--- 4/4.2.py
+++ 4/4.2.py
@@ -1,33 +1,35 @@
 num1 = float(input("Enter first number : "))
 action = input("What we gonna do? : ")
 num2 = float(input("Enter second number : "))
+result = 0
 
 if action == "+":
     result = num1 + num2
     print("Result: ")
-    print(result)
+
 
 elif action == "-":
     result = num1 - num2
     print("Result: ")
-    print(result)
+
 
 elif action == "/":
     result = num1 / num2
     print("Result: ")
-    print(result)
+
 
 elif action == "*":
     result = num1 * num2
     print("Result: ")
-    print(result)
+
 
 elif action == "**":
     result = num1 ** num2
     print("Result: ")
-    print(result)
+
 
 elif action == "%":
     result = num1 % num2
     print("Result: ")
-    print(result)
+
+print(result)
