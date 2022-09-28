28f0cbe6ae556f272606fee23a2ddbac38079cdb
Ridex <ridexGumesPresent@gmail.com>
2022-09-28 17:06:04 +0300

    4.5

--- 4/4.5.py
+++ 4/4.5.py
@@ -1,11 +1,11 @@
-Number2 = int(input("Enter the range number: "))
 counter = 0
 s = ""
 
-for num in range(1, Number2 + 1):
+for num in range(1, 4):
     for
     if num % 1 == 0:
         counter += 1
+        num < 10
         s += str(num) + " "
 
         if counter > 3:
