9bd678b2c57e35d2f5ed77480110db5d61c394e0
Ridex <ridexGumesPresent@gmail.com>
2022-09-28 17:18:10 +0300

    4.5

--- 4/4.5.py
+++ 4/4.5.py
@@ -1,14 +1,11 @@
-counter = 0
 s = ""
 num = 1
 for col in range(1, 4):
     for row in range(1, 4):
-        counter += 1
-        num < 10
         s += str(num) + " "
         num += 1
 
-        if counter > 3:
+        if num < 10:
             print(s)
             s = ""
-            counter = 0
+
