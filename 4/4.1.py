b4ad7f4c56fce409bff4cde6369901bcf2f30bed
Ridex <ridexGumesPresent@gmail.com>
2022-09-28 16:19:19 +0300

    Final 4.1

--- 4/4.1.py
+++ 4/4.1.py
@@ -2,7 +2,6 @@ N = int(input("Enter the first number: "))
 M = int(input("Enter second number: "))
 K = int(input("Enter last number: "))
 
-if N % M == 0 and N > K:
-    print(N)
-else:
-    print("error")
+for num in range(1, N+1):
+    if num % M == 0 and num > K:
+        print(num)
