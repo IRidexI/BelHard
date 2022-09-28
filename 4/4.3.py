b5b12731f8466f6af3aa03abe8056e3ed5947ffb
Ridex <ridexGumesPresent@gmail.com>
2022-09-28 15:16:40 +0300

    add 4.3

--- 4/4.3.py
+++ 4/4.3.py
@@ -1,8 +1,7 @@
-Number = 2
 Number2 = int(input("Enter the range number: "))
 counter = 0
 
-for num in range(Number, Number2 + 1):
+for num in range(2, Number2 + 1):
     if num % 2 == 0:
         counter += 1
         print(num)
