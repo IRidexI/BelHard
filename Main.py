2181ed109ecdb7705ec6b6e24b73d5a919753300
Ridex <ridexGumesPresent@gmail.com>
2022-10-02 18:52:11 +0300

    main

---
+++ Main.py
@@ -0,0 +1,21 @@
+# 1
+
+# number = input()
+# numbers = list(map(int, number))
+# print(numbers)
+
+
+# 2
+
+# text = input().split('. ')
+#
+# word_count = list(map(lambda x: x.count(' ') + 1, text))
+# print(word_count)
+
+# 3
+
+# text = 'wdijwnkfasknlfamk1234sd'
+#
+# numbers = filter(lambda x: x.isdigit(), text)
+# numbers = map(int, numbers)
+# print(sum(numbers))
