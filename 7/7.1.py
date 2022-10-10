e1b3adddf3ec29da2effcdec5ec3828e704d8773
Ridex <ridexGumesPresent@gmail.com>
2022-10-10 08:13:47 +0300

    7.1

--- 7/7.1.py
+++ 7/7.1.py
@@ -1,5 +1,8 @@
 class Time:
 
     def __init__(self, start_time: str, end_time: str,  delta: str):
+        self.start_time = '10:00'
+        self.end_time = '19:00'
+        self.delta = '0:30'
 
 
